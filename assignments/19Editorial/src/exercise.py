a709b4ff4da7e899612fce4d279ce92a3b424e21
A01412414 <a01412414Qitesm.mx>
2021-08-28 12:44:13 -0500

    Sabado Editorial edit

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,7 +1,17 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
-
+    palabras = int(input("Dame el número de palabras: "))
+    pag = palabras / 450
+    unapag = 60
+    dospag = 120
+    if pag <= 1:
+        pagina = unapag - (unapag * .1)
+        print("El costo de la publicación es:", pagina)
+    else:
+        paginas = dospag - (dospag * .1)
+        print("El costo de la publicación es:", paginas)
+  
 
 if __name__ == '__main__':
     main()
+    
