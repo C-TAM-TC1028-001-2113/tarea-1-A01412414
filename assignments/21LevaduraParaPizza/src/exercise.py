f2a51584549e11b572419e70dddff94c28db1b53
A01412414 <a01412414Qitesm.mx>
2021-08-28 13:20:17 -0500

    Sabado Levadura pza

--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -1,7 +1,9 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
-
+    gramos = float(input("Dame la harina en gramos: "))
+    levadura = gramos // 20
+    print("Necesitas estos gramos de levadura:", levadura)
 
 if __name__ == '__main__':
     main()
+    
