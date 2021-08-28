7c8ab63e1d380fb6e1b06a967605e9b73fa9ba9e
A01412414 <a01412414Qitesm.mx>
2021-08-28 13:10:50 -0500

    Editorial edit Sabado

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,17 +1,12 @@
+from math import ceil
+
 def main():
     # escribe tu código abajo de esta línea
     palabras = int(input("Dame el número de palabras: "))
-    pag = palabras / 450
-    unapag = 60
-    dospag = 120
-    if pag <= 1:
-        pagina = unapag - (unapag * .1)
-        print("El costo de la publicación es:", pagina)
-    else:
-        paginas = dospag - (dospag * .1)
-        print("El costo de la publicación es:", paginas)
+    pag = ceil(palabras / 450)
+    total = (pag * 60) * 0.9
+    print("El costo de la publicación es: ", total)
   
 
 if __name__ == '__main__':
     main()
-    
